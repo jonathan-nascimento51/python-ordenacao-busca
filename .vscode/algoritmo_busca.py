388fb563ba2e3e5cf22500df5d0983e54282f4d8
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2023-08-29 12:18:33 -0300

    Exemplo busca sequencial em lista ordenada

--- .vscode/algoritmo_busca.py
+++ .vscode/algoritmo_busca.py
@@ -14,3 +14,25 @@ def busca_sequencial(lista, elemento):
 testelista = [2, 10, 8, 15, 18, 20, 12, 1]
 print(busca_sequencial(testelista, 5))
 print(busca_sequencial(testelista, 15))
+
+
+#Busca Sequencial Ordenada:
+def busca_sequencial_ordenada(lista, elemento):
+    """Busca Sequencial Ordenada:"""
+    pos = 0
+    encontrado = False
+    para = False
+    while pos < len(lista) and not encontrado and not para:
+        if lista[pos] == elemento:
+            encontrado = True
+        else:
+            if lista[pos] > elemento:
+                para = True
+            else:
+                pos = pos+1
+
+    return encontrado
+
+testelista = [1, 2, 8, 10, 13, 15, 18, 20]
+print(busca_sequencial_ordenada(testelista, 5))
+print(busca_sequencial_ordenada(testelista, 15))
