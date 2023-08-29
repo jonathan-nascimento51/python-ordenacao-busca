20fa4b14609a42ec1aad19e705a14d88cb45af6e
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2023-08-29 12:39:21 -0300

    organizando os codigos por pastas de ordenação e busca(algoritmos) e subindo exemplo de ordenação por inserção

---
+++ ordenacao/selection_sort.py
@@ -0,0 +1,17 @@
+"""Selection sort: (Classificação por inserção)"""
+def executar_insertion_sort(lista):
+    """Selection sort: (Classificação por inserção)"""
+    tamanho = len(lista)
+    for i in range(1, tamanho):
+        valor_inserir = lista[i]
+        j = i - 1
+
+        while j >= 0 and lista[j] > valor_inserir:
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = valor_inserir
+
+    return lista
+
+lista1 = [10, 8, 7, 3, 2, 1]
+executar_insertion_sort(lista1)
